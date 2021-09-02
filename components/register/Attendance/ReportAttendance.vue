c748b761a505bd5f0528bf802aac161c4e0f385e
Renan Andrade <Fel9.renan02@gmail.com>
2021-09-02 16:11:29 -0300

    Criado página de reporte de atendimento

---
+++ components/register/Attendance/ReportAttendance.vue
@@ -0,0 +1,143 @@
+<template>
+  <section>
+    <div class="block">
+      <div class="title">Admin / atendimento</div>
+    </div>
+    <h2 class="subtitle">Todos os meus atendimentos</h2>
+
+    <div class="level">
+      <span class="level-left">
+        <b-input
+          class="level-item"
+          type="search"
+          icon="magnify"
+          icon-clickable
+          placeholder="Pesquise na lista"
+        />
+      </span>
+      <span class="level-right">
+        <b-button
+          @click="reportAttendance(false)"
+          type="is-primary"
+          class="level-item is-primary"
+        >
+          Todos os atendimentos
+        </b-button>
+      </span>
+    </div>
+    <b-table
+      :data="data"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+      :pagination-simple="isPaginationSimple"
+      :pagination-position="paginationPosition"
+      :default-sort-direction="defaultSortDirection"
+      :pagination-rounded="isPaginationRounded"
+      :sort-icon="sortIcon"
+      :hoverable="hoverable"
+      :focusable="focusable"
+      :scrollable="scrollable"
+    >
+      <template v-slot="props">
+        <b-table-column
+          v-for="(column, index) in columns"
+          :key="index"
+          :field="column.field"
+          :label="column.label"
+          :sortable="sortable"
+        >
+          {{ props.row[column.field] }}
+        </b-table-column>
+        <!-- <b-table-column custom-key="actions" label="Ações">
+          <b-field>
+            <b-button
+              type="is-primary"
+              icon-left="pencil"
+              @click="editAttendance(props.row)"
+            ></b-button>
+          </b-field>
+          <b-field>
+            <b-button
+              type="is-danger"
+              icon-left="delete"
+              @click="confirmCustomDelete(props.row)"
+            ></b-button>
+          </b-field>
+        </b-table-column> -->
+      </template>
+    </b-table>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      // propriedades da tabela
+      sortable: true,
+      hoverable: true,
+      focusable: true,
+      scrollable: true,
+      isPaginated: true,
+      isPaginationSimple: false,
+      isPaginationRounded: false,
+      paginationPosition: "bottom",
+      defaultSortDirection: "asc",
+      sortIcon: "arrow-up",
+      sortIconSize: "is-small",
+      currentPage: 1,
+      perPage: 5,
+      // término propriedades da tabela
+      data: [],
+      columns: [
+        {
+          field: "id",
+          label: "ID",
+        },
+        {
+          field: "attendance_reason",
+          label: "Razão do atendimento",
+        },
+        {
+          field: "attendance_severity",
+          label: "Gravidade",
+        },
+        {
+          field: "student",
+          label: "Estudante",
+        },
+        {
+          field: "account_attendance",
+          label: "Atendente",
+        },
+        {
+          field: "status",
+          label: "Status",
+        },
+        {
+          field: "opened_at",
+          label: "Aberto em",
+        },
+        {
+          field: "closed_at",
+          label: "Fechado em",
+        },
+      ],
+    };
+  },
+  created() {
+    this.fetchAllAttendances();
+  },
+  methods: {
+    async fetchAllAttendances() {
+      this.data = await this.$axios.$get("/api/v1/attendance/report/");
+    },
+    reportAttendance(value) {
+      this.$emit("reportAttendance", value);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
