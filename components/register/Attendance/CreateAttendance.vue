1786920eecf9d6d9d52e4a76b27a2266a0f8e1f3
Renan Andrade <Fel9.renan02@gmail.com>
2021-10-15 11:48:15 -0300

    Adicionado taglist para atendentes e estudante

--- components/register/Attendance/CreateAttendance.vue
+++ components/register/Attendance/CreateAttendance.vue
@@ -22,21 +22,20 @@
           </b-field>
 
           <b-field label="Atendentes" :label-position="labelPosition">
-            <b-input
-              list="attendants"
-              v-model="attendant"
-              id="attendant"
-              name="attendants"
-              placeholder="Nome da atendente"
-              required
-            />
-            <datalist id="attendants">
-              <option
-                v-for="(item, index) in attendantsAccounts"
-                :key="index"
-                :value="item.full_name"
-              ></option>
-            </datalist>
+            <b-taginput
+              v-model="attendants"
+              id="attendantInput"
+              field="full_name"
+              :data="filteredAccounts"
+              placeholder="Nome do atendente"
+              @typing="searchAccounts()"
+              autocomplete
+            >
+              <template v-slot="props">
+                <strong>{{ props.option.id }}</strong
+                >: {{ props.option.full_name }}
+              </template>
+            </b-taginput>
           </b-field>
 
           <b-field label="Gravidade" :label-position="labelPosition">
@@ -55,21 +54,22 @@
           </b-field>
 
           <b-field label="Estudante" :label-position="labelPosition">
-            <b-input
-              list="students"
+            <b-taginput
               v-model="student"
-              id="student"
-              name="students"
-              placeholder="Nome do aluno"
+              id="studentInput"
+              field="full_name"
+              maxtags="1"
+              :data="filteredStudents"
+              placeholder="Nome do estudante"
               required
-            />
-            <datalist id="students">
-              <option
-                v-for="(item, index) in studentsAccount"
-                :key="index"
-                :value="item.full_name"
-              ></option>
-            </datalist>
+              autocomplete
+              @typing="searchStudents()"
+            >
+              <template v-slot="props">
+                <strong>{{ props.option.id }}</strong
+                >: {{ props.option.full_name }}
+              </template>
+            </b-taginput>
           </b-field>
 
           <b-field label="Status" :label-position="labelPosition">
@@ -93,13 +93,14 @@
                   @click.prevent="createAttendance()"
                   class="is-primary"
                   expanded
-                  >Cadastrar</b-button
                 >
+                  Cadastrar
+                </b-button>
               </div>
               <div class="column is-one-half">
                 <b-button
                   type="is-primary is-outlined"
-                  @click="cancelAttendance(true)"
+                  @click="cancelAttendance(false)"
                   expanded
                 >
                   Cancelar
@@ -118,45 +119,61 @@ export default {
   data() {
     return {
       labelPosition: "on-border",
+      filteredAccounts: this.attendantsAccounts,
+      filteredStudents: this.filteredStudents,
       attendance_reason: "",
-      attendance_severity: "",
-      attendant: "",
-      student: "",
-      status: "",
-      studentsAccount: "",
-      attendantsAccounts: "",
+      attendance_severity: "L",
+      attendants: [],
+      student: [],
+      status: "O",
     };
   },
   created() {
-    this.fetchAllStudents();
+    this.fetchAllAccounts();
   },
   methods: {
-    searchAccounts() {
-      this.attendantAccountIds = [];
-      for (let index = 0; index < this.attendantsAccounts.length; index++) {
-        if (this.attendantsAccounts[index].full_name == this.attendant) {
-          this.attendantAccountIds.push(this.attendantsAccounts[index].id);
-        }
-      }
-      for (let index = 0; index < this.studentsAccount.length; index++) {
-        if (this.studentsAccount[index].full_name == this.student) {
-          this.studentSelected = this.studentsAccount[index].id;
-        }
-      }
-    },
-    async fetchAllStudents() {
+    async fetchAllAccounts() {
       this.studentsAccount = await this.$axios.$get("/api/v1/student/");
       this.attendantsAccounts = await this.$axios.$get("/api/v1/account/");
     },
 
+    searchAccounts() {
+      let value = document.getElementById("attendantInput").value;
+      this.filteredAccounts = this.attendantsAccounts.filter((option) => {
+        return (
+          option.full_name
+            .toString()
+            .toLowerCase()
+            .indexOf(value.toLowerCase()) >= 0
+        );
+      });
+    },
+    searchStudents() {
+      let value = document.getElementById("studentInput").value;
+      this.filteredStudents = this.studentsAccount.filter((student) => {
+        return (
+          student.full_name
+            .toString()
+            .toLowerCase()
+            .indexOf(value.toLowerCase()) >= 0
+        );
+      });
+    },
+    getAccountsId() {
+      this.attendantAccountsId = [];
+      for (let index in this.attendants) {
+        this.attendantAccountsId.push(this.attendants[index].id);
+      }
+      this.studentAccountId = this.student[0].id;
+    },
     async createAttendance() {
       try {
-        this.searchAccounts();
+        this.getAccountsId();
         await this.$axios.$post("/api/v1/attendance/", {
           attendance_reason: this.attendance_reason,
           attendance_severity: this.attendance_severity,
-          attendants: this.attendantAccountIds,
-          student: this.studentSelected,
+          attendants: this.attendantAccountsId,
+          student: this.studentAccountId,
           status: this.status,
         });
         this.$buefy.toast.open({
--- components/register/Attendance/SelectAttendance.vue
+++ components/register/Attendance/SelectAttendance.vue
@@ -21,40 +21,78 @@
         </b-field>
 
         <b-field label="Atendentes" :label-position="labelPosition">
-          <b-input
-            type="text"
-            v-model="currentAttendance.attendants"
-            placeholder="Atendentes"
-            required
+          <b-taginput
+            v-model="attendants"
+            id="attendantInput"
+            field="full_name"
+            :data="filteredAccounts"
+            placeholder="Nome do atendente"
+            @typing="searchAccounts()"
+            autocomplete
           >
-          </b-input>
+            <template v-slot="props">
+              <strong>{{ props.option.id }}</strong
+              >: {{ props.option.full_name }}
+            </template>
+          </b-taginput>
         </b-field>
 
+        <!-- <b-field label="Atendentes" :label-position="labelPosition">
+          <b-taginput
+            list="attendants"
+            v-model="currentAttendance.attendants"
+            id="attendant"
+            name="attendants"
+            placeholder="Nome da atendente"
+            required
+          />  
+        </b-field> -->
+
         <b-field label="Estudante" :label-position="labelPosition">
-          <b-input
-            type="number"
-            v-model="currentAttendance.student.id"
+          <b-taginput
+            v-model="student"
+            id="studentInput"
+            field="full_name"
+            maxtags="1"
+            :data="filteredStudents"
             placeholder="Nome do estudante"
             required
+            autocomplete
+            @typing="searchStudents()"
           >
-          </b-input>
+            <template v-slot="props">
+              <strong>{{ props.option.id }}</strong
+              >: {{ props.option.full_name }}
+            </template>
+          </b-taginput>
         </b-field>
 
         <b-field label="Status" :label-position="labelPosition">
-          <b-input
+          <b-select
             type="text"
             v-model="currentAttendance.status"
             placeholder="Status do atendimento"
           >
-          </b-input>
+            <option value="O">Aberto</option>
+            <option value="OH">Em espera</option>
+            <option value="IP">Em andamento</option>
+            <option value="C">Fechado</option>
+          </b-select>
         </b-field>
+
         <b-field label="Gravidade" :label-position="labelPosition">
-          <b-input
+          <b-select
+            class="is-primary"
             type="text"
             v-model="currentAttendance.attendance_severity"
-            placeholder="Gravidade"
+            placeholder="Gravidade do problema"
+            required
           >
-          </b-input>
+            <option value="L">Leve</option>
+            <option value="M">Média</option>
+            <option value="H">Grave</option>
+            <option value="S">Alta</option>
+          </b-select>
         </b-field>
 
         <b-field class="columns">
@@ -74,6 +112,9 @@
           </div>
         </b-field>
       </form>
+      {{ filteredAccounts }} <br />
+      <br />
+      {{ filteredStudents }}
     </div>
   </div>
 </template>
@@ -88,22 +129,74 @@ export default {
     return {
       currentAttendance: {},
       labelPosition: "on-border",
+      studentsAccount: [],
+      attendantsAccounts: [],
+      filteredAccounts: this.filteredAccounts,
+      filteredStudents: this.filteredStudents,
+      attendants: [],
+      student: [],
+      value: [],
     };
   },
   created() {
     this.currentAttendance = this.attendance;
+    this.fillTagInputs();
+    this.fetchAllAccounts();
   },
   methods: {
+    async fetchAllAccounts() {
+      this.studentsAccount = await this.$axios.$get("/api/v1/student/");
+      this.attendantsAccounts = await this.$axios.$get("/api/v1/account/");
+    },
+    fillTagInputs() {
+      this.student.push(this.currentAttendance.student.full_name);
+      for (let index in this.currentAttendance.attendants) {
+        this.attendants.push(
+          this.currentAttendance.attendants[index].full_name
+        );
+      }
+    },
+    searchAccounts() {
+      let value = document.getElementById("attendantInput").value;
+      this.filteredAccounts = this.attendantsAccounts.filter((option) => {
+        return (
+          option.full_name
+            .toString()
+            .toLowerCase()
+            .indexOf(value.toLowerCase()) >= 0
+        );
+      });
+    },
+    searchStudents() {
+      let value = document.getElementById("studentInput").value;
+      this.filteredStudents = this.studentsAccount.filter((student) => {
+        return (
+          student.full_name
+            .toString()
+            .toLowerCase()
+            .indexOf(value.toLowerCase()) >= 0
+        );
+      });
+    },
+    getAccountsId() {
+      this.attendantAccountsId = [];
+      for (let index in this.attendants) {
+        this.attendantAccountsId.push(this.attendants[index].id);
+      }
+      this.studentAccountId = this.student[0].id;
+    },
+
     async atualizar() {
       try {
+        this.getAccountsId();
         await this.$axios.$patch(
           `/api/v1/attendance/${this.currentAttendance.id}/`,
           {
-            id: this.currentAttendance.id,
-            attendants: this.currentAttendance.attendants,
+            id: this.currentAttendance.attendance_reason,
+            attendants: this.attendantAccountsId,
             attendance_reason: this.currentAttendance.attendance_reason,
             attendance_severity: this.currentAttendance.attendance_severity,
-            student: this.currentAttendance.student,
+            student: this.studentAccountId,
             status: this.currentAttendance.status,
           }
         );
