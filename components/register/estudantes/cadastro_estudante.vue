3b90cd6efb553dd592a42e9f1b9324c35cbb6147
RenanAndrade <fel9.renan02@gmail.com>
2021-11-29 14:22:03 -0300

    Criado formulário para students

--- components/register/estudantes/cadastro_estudante.vue
+++ components/register/estudantes/cadastro_estudante.vue
@@ -11,6 +11,7 @@
           icon="calendar-today"
           v-model="ingress_date"
           trap-focus
+          locale="pt-BR"
           width="60%"
         >
         </b-datepicker>
@@ -29,14 +30,16 @@
         </b-input>
       </b-field>
 
-      <b-field label="Responsáveis">
-        <b-input
-          placeholder="Nome dos responsáveis"
+      <b-field label="Adicione os responsáveis">
+        <b-taginput
           v-model="responsables"
-          type="number"
-        />
+          ellipsis
+          icon="label"
+          placeholder="Nome dos responsáveis"
+          aria-close-label="Delete esse item"
+        >
+        </b-taginput>
       </b-field>
-
       <b-field>
         <b-field class="columns">
           <div class="column is-one-half">
@@ -60,37 +63,29 @@ export default {
   data() {
     return {
       labelPosition: "is-centered",
-      ingress_date: [],
+      ingress_date: new Date(),
       academic_education_campus: "",
       person: "",
       registration: "",
-      responsables: "",
+      responsables: [],
     };
   },
-  computed: {
-    sampleFormat() {
-      const dtf = new Intl.DateTimeFormat("pt-BR");
-      console.log(dtf.format(this.ingress_date));
-      return dtf.format();
-    },
-  },
   methods: {
-    responsablesArray(responsables) {
-      const responsablesArray = [];
-      return responsablesArray.push(responsables);
+    formatDate(date) {
+      var dateArray = [];
+      dateArray.push(date.getDate(), date.getMonth() + 1, date.getFullYear());
+
+      return dateArray.reverse().join("-");
     },
-
     async createStudent() {
+      var date = this.formatDate(this.ingress_date);
       try {
-        const responsables = this.responsablesArray(this.responsables);
-        const date = this.reversedDate(this.ingress_date);
-
         await this.$axios.$post("/api/v1/student/", {
           person: this.person,
           academic_education_campus: this.academic_education_campus,
-          responsibles: responsables,
+          responsibles: this.responsables,
           registration: this.registration,
-          ingress_date: this.ingress_date,
+          ingress_date: date,
         });
       } catch (error) {
         console.log(error);
